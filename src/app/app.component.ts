2b3c6b4208cf3f8be0a877afa7d01b60c0cb0282
jehadja <jehad.jaghoub2@gmail.com>
2017-06-17 03:30:09 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,6 +41,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.authService.startupTokenRefresh();
     });
   }
 
